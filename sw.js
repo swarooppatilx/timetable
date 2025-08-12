e210399b4db33a2716aafb5cd946677d18cced8b
swarooppatilx <swarooppatil850@gmail.com>
2025-08-12 11:24:00 +0530

    feat: update offline strategy

--- sw.js
+++ sw.js
@@ -6,58 +6,34 @@ const urlsToCache = [
     '/app.js',
     '/data.js',
     '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+    '/icons/icon-512x512.png'
+];
+
+const externalAssets = [
     'https://cdn.tailwindcss.com',
     'https://unpkg.com/lucide@latest'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache and caching app shell');
-                return cache.addAll(urlsToCache);
-            })
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            await cache.addAll(urlsToCache);
+            for (const url of externalAssets) {
+                try {
+                    const response = await fetch(url, { mode: 'no-cors' });
+                    await cache.put(url, response);
+                } catch (err) {
+                    console.error('Failed to cache external asset:', url, err);
+                }
+            }
+        })()
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(
-                    (response) => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+            .then(response => response || fetch(event.request))
     );
 });
